742e97db716e2b2b7d37ddb9dfdd1809fc2d0f3e
Yassine <yassine.assouale@gmail.com>
2021-12-16 02:01:47 +0100

    Creating Spring MVC Project - Pom xml

---
+++ src/main/java/ya/springboot/sms/SpringMvcFormationApplication.java
@@ -0,0 +1,13 @@
+package ya.springboot.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcFormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcFormationApplication.class, args);
+	}
+
+}
